cd774583f76f207668d37c1f121a53bfd4ff299c
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-17 15:53:32 +0100

    Fatto layout header per bonus Dadi

---
+++ bonus/Dadi - partita a 5/js/script.js
@@ -0,0 +1,16 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Creo il numero random per utente e computer
+// const userNumber = Math.floor(Math.random() * 6) + 1;
+// const computerNumber = Math.floor(Math.random() * 6) + 1;
+
+// Imposto la logica della vittoria/sconfitta/pareggio dell'utente col relativo output
+// if(userNumber > computerNumber){
+//     alert(`Hai vinto (Tu: ${userNumber}, Computer: ${computerNumber})`);
+// } else if(userNumber < computerNumber){
+//     alert(`Hai perso (Tu: ${userNumber}, Computer: ${computerNumber})`);
+// } else{
+//     alert(`Hai pareggiato (Tu: ${userNumber}, Computer: ${computerNumber})`);
+// }
